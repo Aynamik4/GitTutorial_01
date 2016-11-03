c282108b47ae5c6244bf7b033bce896626e1a871
Aynamik4 <violintailgut@gmail.com>
2016-11-03 14:25:44 +0100

    Update Program.cs

--- GitTutorial_01_Project/Program.cs
+++ GitTutorial_01_Project/Program.cs
@@ -7,6 +7,7 @@ namespace GitTutorial_01_Project
         static void Main(string[] args)
         {
             WriteLine("Hello GitTutorial_01! Still not on Git...");
+            WriteLine("This change made on github.com");
         }
     }
 }
