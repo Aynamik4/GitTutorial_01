d0681390fa13089efe74a263ab6d67b0d319f6bb
Aynamik4 <violintailgut@gmail.com>
2016-11-03 13:22:27 +0100

    Add project files.

---
+++ GitTutorial_01_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace GitTutorial_01_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello GitTutorial_01");
+        }
+    }
+}
