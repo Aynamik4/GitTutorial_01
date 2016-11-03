a7d8842f8d4eac1639bd0bb50d0a82395335e963
Aynamik4 <violintailgut@gmail.com>
2016-11-03 16:05:40 +0100

    Committing revert!

--- GitTutorial_01_Project/Program.cs
+++ GitTutorial_01_Project/Program.cs
@@ -6,10 +6,7 @@ namespace GitTutorial_01_Project
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello GitTutorial_01! Still not on Git...");
-            WriteLine("This change made on github.com");
-            WriteLine("This change also made on github.com");
-            WriteLine("This entered via VS 2015");
+            WriteLine("Hello GitTutorial_01");
         }
     }
 }
